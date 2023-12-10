78e471e553dd84c0a15c306dd1ed3d29e3cad2d5
Dang Vip Pro <ntdanga22032@cusc.ctu.edu.vn>
2023-12-11 05:36:40 +0700

    first commit

---
+++ AMT_Company/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AMT_Company.Models
+{
+    public class User
+    {
+        [Key]
+        [Required(ErrorMessage = "Username cannot be blank")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password cannot be blank")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "ConfirmPassword cannot be blank")]
+        [NotMapped]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Fullname")]
+        [Required(ErrorMessage = "Fullname cannot be blank")]
+        public string Fullname { get; set; }
+
+        [Display(Name = "Birthday")]
+        [Required(ErrorMessage = "Birthday cannot be blank")]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Avatar")]
+        [Required(ErrorMessage = "Avatar cannot be blank")]
+        public string Avatar { get; set; }
+
+        [Display(Name = "Gender")]
+        [Required(ErrorMessage = "Gender cannot be blank")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email cannot be blank")]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone")]
+        [Required(ErrorMessage = "Phone cannot be blank")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Address cannot be blank")]
+        public string Address { get; set; }
+    }
+}
---
+++ AMT_Company/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AMT_Company")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AMT_Company")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ed3c63e-fb26-47f4-aac2-a34dc201eff1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
